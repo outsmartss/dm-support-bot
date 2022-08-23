fbfc47babf35cd534c219b9a4c1e1c7d3b4e6180
MistakenScripter <korenhoftim@gmail.com>
2022-08-23 12:36:12 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  Client: {
+    TOKEN: "MTAxMTIwMzk3MDMwOTU3MDYyMA.G-2ann.QQmejx29mpvoNgzjfdKhI23zkHiirTZgkHGx0A",
+    ID: "1011203970309570620"
+  },
+
+  Handler: {
+    GUILD_ID: "1011362650204147822",
+    CATEGORY_ID: "1011376028754640946"
+  },
+
+  Modmail: {
+    INTERACTION_COMMAND_PERMISSIONS: ['Administrator']
+  }
+};
